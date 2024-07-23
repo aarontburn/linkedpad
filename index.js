8f3629301be66f73cf5ba15c83f723cd746381ce
Aaron <aarontburnham@hotmail.com>
2024-07-22 18:26:22 -0700

    yea

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient } from "mongodb";
 
-const KEYS = (() => { // ["A1", "A2", ..., "D4"]
+const ACCESS_QUERY = { 'accessID': ':3' }
+
+// ["A1", "A2", ..., "D4"]
+const KEYS = (() => { 
     const rows = ["A", "B", "C", "D"];
     const columns = ["0", "1", "2", "3"];
 
@@ -14,7 +17,19 @@ const KEYS = (() => { // ["A1", "A2", ..., "D4"]
     return out;
 })();
 
-const ACCESS_ID = ':3';
+// { "A0": 0, "A1": 0, ..., "D4": 0, "accessID": ":3" }
+const DEFAULT_DB_OBJ = (() => {
+    const object = {};
+    for (const key of KEYS) {
+        object[key] = 0;
+    }
+    const key = Object.keys(ACCESS_QUERY)[0];
+    object[key] = ACCESS_QUERY[key];
+    return object;
+})();
+
+const localState = {};
+
 const URI = "mongodb+srv://admin:j2MzVYcewmPjnzrG@linkedpad.qrzkm98.mongodb.net/?retryWrites=true&w=majority&appName=linkedpad";
 
 
@@ -23,29 +38,28 @@ const database = client.db("pad_data");
 const collection = database.collection("data");
 
 
+
 async function initialize() {
     console.log("Initializing...");
-    await checkDatabase();
 
-    collection.findOne(new ObjectId('669724e26784a639c2fdaed0')).then(async data => {
-        listen().catch(console.dir);
+    checkDatabase().then(() => {
+        collection.findOne({}).then(async data => {
+            await recalibrate();
+            listen().catch(initialize); // Reboot if error
+        });
     });
 }
-
-
-const VALID_KEYS = [...KEYS, '_id', 'accessID'];
 
 async function checkDatabase() {
     if (await collection.estimatedDocumentCount() === 1) {
         const entry = await getObject();
 
         if (entry !== null) {
-            if (Object.keys(entry).sort().toString() === VALID_KEYS.sort().toString()) { // All keys are valid
+            if (Object.keys(entry).sort().toString() === [...KEYS, '_id', 'accessID'].sort().toString()) { // All keys are valid
                 console.log("Database is properly initialized.");
                 return;
             }
         }
-
     }
 
     console.log("WARNING: Database needs to be re-setup.");
@@ -53,10 +67,12 @@ async function checkDatabase() {
     await reset();
 }
 
+/**
+ *  Listens to any database changes.
+ */
 async function listen() {
-    let changeStream;
     try {
-        changeStream = collection.watch();
+        const changeStream = collection.watch();
         console.log("Listening for changes...");
 
         // Print change events as they occur
@@ -64,7 +80,6 @@ async function listen() {
             if (change && change.updateDescription && change.updateDescription.updatedFields) {
                 onDatabaseChange(change.updateDescription.updatedFields)
             }
-
         }
         await changeStream.close();
 
@@ -73,42 +88,87 @@ async function listen() {
     }
 }
 
-async function onDatabaseChange(changeObject) {
-    const row = Object.keys(changeObject);
-    const unparsedData = changeObject[row];
-    console.log(row);
-    console.log(changeObject[row]);
 
-    for (let i = 0; i < unparsedData.length; i++) {
-        if (unparsedData > 0) {
-            // Turn light on
-
+/**
+ *  An event that triggers when the database object is modified.
+ *  @param changeObject     An object containing all keys and new states for each button.
+ */
+function onDatabaseChange(changeObject) {
+    for (const rowCol in changeObject) {
+        if (!KEYS.includes(rowCol)) { // Maybe not needed?
             continue;
         }
-        // Turn light off
+
+        const row = rowCol[0];
+        const col = rowCol[1];
+        const newValue = changeObject[rowCol];
+        setLight(row, col, newValue);
     }
 }
 
-
-
+/**
+ *  Reset all buttons to 0
+ */
 async function reset() {
-    // Iterate over A0, A1, ... D3, D4 and reset their entries to 0
-    const object = {};
-    for (const key of KEYS) {
-        object[key] = 0;
+    const result = await collection.findOneAndUpdate(
+        ACCESS_QUERY,
+        { '$set': DEFAULT_DB_OBJ }, { returnDocument: 'after', upsert: true });
+
+    for (const key in result) {
+        setLight(key[0], key[1], 0);
     }
-    object['accessID'] = ACCESS_ID;
-    await insertObjectIntoDatabase(object);
 }
 
-async function insertObjectIntoDatabase(object) {
-    await collection.replaceOne({ 'accessID': ACCESS_ID }, object, { upsert: true });
+/**
+ *  Recalibrates all lights to the state reflected in the database.
+ */
+async function recalibrate() {
+    const currentState = await getObject();
+
+    for (const key of KEYS) {
+        setLight(key[0], key[1], currentState[key]);
+    }
 }
+
 
 async function getObject() {
-    return await collection.findOne({ 'accessID': ACCESS_ID });
+    return await collection.findOne(ACCESS_QUERY);
 }
 
-// async function updateDatabase(row, )
+
+function setLight(row, col, isOn) {
+    localState[row + col] = isOn;
+    displayStateToConsole();
+    // TODO: Implement the actual light toggle
+}
+
+
+// Flips whatever the button position is.
+async function onClick(row, col) {
+    await collection.findOneAndUpdate(
+        ACCESS_QUERY,
+        { "$bit": { [row + col]: { 'xor': 1 } } }
+    )
+}
+
+function displayStateToConsole() {
+    let s = '';
+
+    for (let i = 0; i < KEYS.length; i++) {
+        if (i % 4 === 0) {
+            s += "\n";
+        }
+
+        const t = localState[KEYS[i]];
+
+        if (t === undefined) {
+            s += '0 ';
+        } else {
+            s += t > 0 ? '1 ' : '0 ';
+        }
+    }
+
+    console.log(s);
+}
 
 initialize();
