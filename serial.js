428efddc878fb01933ec58341c54ada1e2279615
Aaron <aarontburnham@hotmail.com>
2024-07-27 18:51:29 -0700

    Update serial.js

--- serial.js
+++ serial.js
@@ -1,10 +1,8 @@
-import { SerialPort } from 'serialport';
 import * as jf from 'johnny-five';
 
 
-
-const PIN_1 = new jf.Pin(3);
-const PIN_2 = new jf.Pin(5);
+const PIN_1 = new jf.Pins(3);
+const PIN_2 = new jf.Pins(5);
 
 PIN_1.read((error, value) => {
     console.log(value)
