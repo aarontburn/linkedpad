5b4e5fa4f47fb59faf2fba6d727a80adfd5c6e7c
Aaron <aarontburnham@hotmail.com>
2024-07-28 16:02:20 -0700

    Update serial.py

--- serial.py
+++ serial.py
@@ -7,17 +7,18 @@ pin1 = 11
 def setup():
   GPIO.setmode(GPIO.BOARD)       
   
-  GPIO.setup(pin, GPIO.IN)   # Set LedPin's mode is output
-  GPIO.setup(pin1, GPIO.OUT)   # Set LedPin's mode is output
+  GPIO.setup(pin, GPIO.IN)
+  GPIO.setup(pin1, GPIO.IN)
   
 
 def loop():
   while True:
-    print(GPIO.input(pin))
+    print(pin + " " + GPIO.input(pin))
+    print(pin1 + " " + GPIO.input(pin1))
+    print()
     time.sleep(0.2)
 
 def destroy():
-  GPIO.output(pin1, GPIO.LOW)   # led off
   GPIO.cleanup()                  # Release resource
 
 if __name__ == '__main__':     # Program start from here
