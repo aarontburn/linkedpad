6a33cf79093ee1daac02168ca7f35766f2c4180e
Aaron <aarontburnham@hotmail.com>
2024-07-27 20:10:06 -0700

    Update serial.js

--- serial.js
+++ serial.js
@@ -1,9 +1,15 @@
 import { Gpio } from "pigpio";
 
+const HIGH = 1;
+const LOW = 0;
+
 
 
 const pin1 = new Gpio(3, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP });
 const pin2 = new Gpio(5, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP });
+
+pin2.digitalWrite(HIGH)
+
 
 
 pin1.on('interrupt', (level) => {
