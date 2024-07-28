fd32c83460a913b4fa42940c91159e431b8ac4ea
Aaron <aarontburnham@hotmail.com>
2024-07-27 20:28:22 -0700

    Update serial.js

--- serial.js
+++ serial.js
@@ -3,7 +3,7 @@ import { Gpio } from "pigpio";
 const HIGH = 1;
 const LOW = 0;
 
-const pin = new Gpio(40, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP });
+const pin = new Gpio(40);
 
 console.log("Listening...")
 
