7ee01e15fbe22d75a291f3f3a8da76a591fd6851
Aaron <aarontburnham@hotmail.com>
2024-07-28 21:53:32 -0700

    yea

--- GPIOHandler.py
+++ GPIOHandler.py
@@ -1,14 +1,16 @@
 import RPi.GPIO as GPIO
-import DatabaseHandler
 import time
+from Key import Key
 
-
-
+def setup_keys() -> dict[str, Key]:
+	out: dict[str, Key] = {}
+	out['A3'] = Key(3, 5, "A3");
+	
+key_map: dict[str, Key] = setup_keys()
 
 # GPIO setup
 INPUT_PIN = 3
 OUTPUT_PIN = 5
-
 
 
 def setup_gpio():
@@ -18,34 +20,23 @@ def setup_gpio():
 	GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(OUTPUT_PIN, GPIO.OUT)
 	GPIO.output(OUTPUT_PIN, GPIO.LOW)
-	
+	setup_keys()
+ 
 	print("GPIO initialization finished.")
+
+
 
 def gpio_listen():
 	print("GPIO listener started.")
  
-	pressed = False
 	while True:
-		if pressed == True:
-			if GPIO.input(INPUT_PIN) == 0:	# Hold
-				print("Holding")
-			else:							# Release
-				print("Key Up")
-				pressed = False
-		else:
-			if GPIO.input(INPUT_PIN) == 0:	# Down
-				print("Key Down")
-				pressed = True
-
-			
-		# if GPIO.input(INPUT_PIN) == 0:
-		# 	DatabaseHandler.on_key_press('A', '3')
-			
+		for row_col in key_map:
+			key: Key = key_map[row_col]
+			key.handle_input(GPIO.input(key._input_pin))
 			
 
 def destroy_gpio():
 	GPIO.cleanup()
 
 
-def _milli():
-	return round(time.time() * 1000)
+
---
+++ Key.py
@@ -0,0 +1,38 @@
+import DatabaseHandler
+
+class Key:
+
+	_pressed: bool = False
+
+	_input_pin: int
+	_output_pin: int
+
+	# Could be reduced to rowCol ('A3')
+	_row: str
+	_col: str
+
+
+	def __init__(self, input_pin: int, output_pin: int, row_col: str) -> None:
+		self._input_pin = input_pin
+		self._output_pin = output_pin
+		self._row = row_col[0]
+		self._col = row_col[1]
+
+	def handle_input(self, input: int):
+		if self._pressed == True:
+			if input == 0:					# Hold
+				print("Holding")
+			else:							# Release
+				print("Key Up")
+				self._pressed = False
+		else:
+			if input == 0:					# Down
+				print("Key Down")
+				DatabaseHandler.on_key_press(self._row, self._col)
+				self._pressed = True
+
+
+	
+
+
+
