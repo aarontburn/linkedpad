c5bd32a005b012fead79fb3e273ff3b24850cf35
Aaron <aarontburnham@hotmail.com>
2024-08-11 19:24:46 -0700

    yea

--- serial/Serial.py
+++ serial/Serial.py
@@ -1,20 +1,36 @@
 import serial
 
-ser = serial.Serial(
-    port='/dev/ttyGS0',
-    baudrate = 9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-)
 
-counter = 0
+PORT: str = '/dev/ttyGS0'
+BAUD: int = 9600
 
 
-print("Listening to serial: ")
-while 1:
-    x = ser.readline()
-    print(x)
-        
-        
+ser: serial.Serial = None
+
+
+
+
+def main():
+    ser = serial.Serial(
+        port=PORT,
+        baudrate = BAUD,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
+    
+    print("Listening")
+    
+
+    while 1:
+        x = str(ser.readline())
+        print(x)
+            
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Closing serial connection.")
+        ser.close()
