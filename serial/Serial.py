7fe33cb85094acf580726d61d9b6e8901a6f60b2
Aaron <aarontburnham@hotmail.com>
2024-08-11 19:38:28 -0700

    Update Serial.py

--- serial/Serial.py
+++ serial/Serial.py
@@ -31,6 +31,7 @@ def main():
 if __name__ == '__main__':
     try:
         main()
-    except Exception:
+    except Exception as e:
+        print(e)
         print("Closing serial connection.")
         ser.close()
