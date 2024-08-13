12746fcd4ec5e8b651dffbb78839788a09fd3822
Aaron <aarontburnham@hotmail.com>
2024-08-12 22:14:35 -0700

    Update DatabaseHandler.py

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -129,6 +129,7 @@ def _display_to_console() -> None:
             s += str(ColorHandler.rgb_to_hex(t)) + " "
         except KeyError:
             s += '#ZZZZZZ '
+    SerialPi.send(s)
     print(s)
     
 
