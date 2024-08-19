bce054b47caa7e59401f98a7f211c9d2d8151ea1
Aaron <aarontburnham@hotmail.com>
2024-08-19 13:09:08 -0700

    Update SerialHandler.py

--- SerialHandler.py
+++ SerialHandler.py
@@ -26,7 +26,7 @@ def _attempt_connection() -> None:
         data: str = str(_ser.readline())[2:-3]
         if data == 'pc_ready':
             log('Successfully established connection with PC.')
-            _start_thread(maintain_connection)
+            # _start_thread(maintain_connection)
             LEDHandler.cleanup()
             
             global _is_connected
