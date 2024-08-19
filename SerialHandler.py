5bed967933dff55e9a93d73c3bb69343ac769486
Aaron <aarontburnham@hotmail.com>
2024-08-19 13:32:58 -0700

    Update SerialHandler.py

--- SerialHandler.py
+++ SerialHandler.py
@@ -77,10 +77,10 @@ def _handle_events(event_string: str) -> None:
             rgb: list[int] = json.loads(split_str[2])
             LEDHandler.set_light(row_col, rgb)
         
-        case 'mode':
-            log("Mode:", split_str[0])
+        case 'linked-mode':
+            log("linked-mode:", split_str[0])
         
-         case _:
+        case _:
             log('No handler for: ' + split_str[0])
 
 
