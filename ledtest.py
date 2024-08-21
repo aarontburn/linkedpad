99f2be9a8539b5dd5fbe9f6a24835490f11b21e1
Aaron <aarontburnham@hotmail.com>
2024-08-20 19:11:25 -0700

    Update ledtest.py

--- ledtest.py
+++ ledtest.py
@@ -2,14 +2,16 @@ import neopixel
 import time
 import board
 
-pixels = neopixel.NeoPixel(board.D18, 1)
+pixels = neopixel.NeoPixel(board.D18, 2)
 
 while True:
     print('white')
     pixels[0] = (255, 255, 255)
+    pixels[1] = (255, 255, 255)
     time.sleep(1)
     print('off')
     pixels[0] = (0, 0, 0)
+    pixels[1] = (0, 0, 0)
     time.sleep(1)
     
     
