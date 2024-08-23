8524e71a0497bd5ecd0fc69c7c583ac1ff9f819e
Aaron <aarontburnham@hotmail.com>
2024-08-22 23:01:27 -0700

    Update GPIOHandler.py

--- GPIOHandler.py
+++ GPIOHandler.py
@@ -14,7 +14,7 @@ key_map: dict[str, Key] = {}
 
 
 KEYS = [row + col for row in ['H', "A", "B", "C", "D"] for col in ["0", "1", "2", "3"]]
-COL_PINS = [37, 35, 33, 31, 29].reverse()
+COL_PINS = list(reversed([37, 35, 33, 31, 29]))
 ROW_PINS = [40, 38, 36, 32]
 
 
