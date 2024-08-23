73020b91846f1cea626900c7b618a1ceced7afcc
Aaron <aarontburnham@hotmail.com>
2024-08-22 23:23:26 -0700

    Update GPIOHandler.py

--- GPIOHandler.py
+++ GPIOHandler.py
@@ -14,7 +14,7 @@ key_map: dict[str, Key] = {}
 
 
 KEYS = [row + col for row in ['H', "A", "B", "C", "D"] for col in ["0", "1", "2", "3"]]
-ROW_PINS = [37, 35, 33, 31, 29] 
+ROW_PINS = [29, 31, 33, 35, 37] 
 COL_PINS = [32, 36, 38, 40]
 
 
@@ -51,7 +51,7 @@ def setup_keys() -> None:
             row = COL_PINS[j]
             key_map[KEYS[i]] = Key(row, col, KEYS[i])
             index += 1
-
+    
             
     
 
