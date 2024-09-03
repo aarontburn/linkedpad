bad0a30db671b7ecac8e6339d9d8cca2238fdbd4
Aaron <aarontburnham@hotmail.com>
2024-09-02 22:55:49 -0700

    Update LEDHandler.py

--- LEDHandler.py
+++ LEDHandler.py
@@ -94,9 +94,14 @@ def do_loading_pattern() -> None:
     pattern: list[str] = ['A0', 'A1', 'A2', 'A3', 'B3', 'C3', 'D3', 'D2', 'D1', 'D0', 'C0', 'B0']
     while True:
         for row_col in pattern:
-            log(row_col)
+            log(int(LIGHT_MAP[row_col]))
+            
+            rgb = ColorHandler.WHITE
+            pixels[int(LIGHT_MAP[row_col])] = (rgb[0], rgb[1], rgb[2])
+            
             set_light(row_col, ColorHandler.WHITE)
             sleep(0.5)
+            rgb = ColorHandler.OFF
             set_light(row_col, ColorHandler.OFF)
             
         
