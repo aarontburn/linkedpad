c02a04f2052a70351b056eeabdfeed023dcfe8f8
Aaron <aarontburnham@hotmail.com>
2024-09-02 23:18:08 -0700

    Update LEDHandler.py

--- LEDHandler.py
+++ LEDHandler.py
@@ -92,6 +92,7 @@ def cleanup() -> None:
 
 def do_loading_pattern() -> None:
     pattern: list[str] = ['A0', 'A1', 'A2', 'A3', 'B3', 'C3', 'D3', 'D2', 'D1', 'D0', 'C0', 'B0']
+    set_brightness(0.5)
     while True:
         for row_col in pattern:
             index: int = int(LIGHT_MAP[row_col])
@@ -99,7 +100,7 @@ def do_loading_pattern() -> None:
             
             pixels[index] = tuple(ColorHandler.WHITE)
             
-            sleep(0.5)
+            sleep(0.25)
             pixels[index] = tuple(ColorHandler.OFF)
 
             
